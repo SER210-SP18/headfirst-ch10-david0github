ba9851f761ddbaa66465bead304cebe439900107
davidnguyen <david.nguyen@quinnipiac.edu>
2018-04-11 13:22:32 -0400

    chapter 9 chapter demo

---
+++ app/src/main/java/ser210/quinnipiac/edu/bitsandpizzas/OrderActivity.java
@@ -0,0 +1,17 @@
+package ser210.quinnipiac.edu.bitsandpizzas;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.ActionBar;
+
+public class OrderActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+}
